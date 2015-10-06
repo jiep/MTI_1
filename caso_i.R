6ed6daa866a3ed1e700fe57c6c63bf87a5b22bdd
José Ignacio Escribano <jiep12345@hotmail.com>
2015-10-06 19:12:15 +0200

    Modificado archivo R

--- caso_i.R
+++ caso_i.R
@@ -1,2 +1,119 @@
+# Seleccionamos el archivo
 filename <- file.choose()
-data <- read.table(filename,sep="\t", fill = TRUE)
+
+# Cargamos los datos del fichero de texto como CSV
+data <- read.csv(filename,sep="\t", header=FALSE, 
+                 col.names=c("name", "bat_1986", "hits_1986", "homeruns_1986", "runs_1986", 
+                             "runs_batted_1986", "walks_1986", "years_major_leagues", "times_at_bat_career", "hits_career", 
+                             "homeruns_career", "runs_career", "runs_batted_career", "walks_career", "league_end_1986", 
+                             "division_end_1986", "team_end_1986", "position_1986", "put_outs_1986", "assits_1986", 
+                             "errors_1986", "annual_salary_1987", "league_beginning_1987", "team_beginning_1987"))
+
+# Seleccionamos las variables que nos interesan: 
+# el número de homeruns como variable discreta, 
+# y el salario como la variable continua
+homeruns <- data$homeruns_1986
+salary <- data$annual_salary_1987
+
+#----------------------------------------------------------------------------------------------
+# Diagramas de la variable discreta
+#----------------------------------------------------------------------------------------------
+
+# Diagrama de barras
+aux <- as.data.frame(table(homeruns))
+ybar <- aux$Freq
+xbar <- as.character(aux$homeruns)
+barplot(ybar, names.arg=xbar, space=3, col=4, main="Diagrama de barras del número de homeruns 
+        durante la temporada 1986", xlab="Número de homeruns", ylab="Frecuencia" )
+abline(h=0)
+
+# Diagrama de sectores
+pie(ybar, labels=xbar, main="Diagrama de sectores del número de homeruns")
+
+#----------------------------------------------------------------------------------------------
+# Diagramas de la variable continua
+#----------------------------------------------------------------------------------------------
+
+# Histograma
+hist(salary, breaks=,main="Histograma del salario durante la temporada 1986", xlab="Salario (en miles de dólares)", ylab="Frecuencia", col=3)
+
+# Diagrama de tallos y hojas
+stem(salary, scale=4)
+
+#----------------------------------------------------------------------------------------------
+# Medidas
+#----------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------
+# Medidas de centralización
+#----------------------------------------------------------------------------------------------
+
+# Media
+
+# Eliminamos los NA, si lo hubiera
+homeruns_mean <- mean(homeruns, na.rm = TRUE)
+salary_mean <- mean(salary, na.rm = TRUE)
+
+# Mediana
+homeruns_median <- median(homeruns, na.rm = TRUE)
+salary_median <- median(salary, na.rm = TRUE)
+
+# Moda
+
+# Definimos una función para la moda
+mode <- function(x) {
+  aux <- which(ybar == max(ybar, na.rm = TRUE))
+  return x[aux]
+}
+
+homeruns_mean <- mode(homeruns);
+
+#----------------------------------------------------------------------------------------------
+# Medidas de posición
+#----------------------------------------------------------------------------------------------
+
+homeruns_quantiles <- quantile(homeruns, c(0.25, 0.75, 0.2, 0.9), na.rm = TRUE)
+salary_quantiles <- quantile(salary, c(0.25, 0.75, 0.2, 0.9), na.rm = TRUE)
+
+#----------------------------------------------------------------------------------------------
+# Medidas de dispersión
+#----------------------------------------------------------------------------------------------
+
+# Cuasivarianza
+homeruns_qvar <- var(homeruns, na.rm = TRUE)
+salary_qvar <- var(salary, na.rm = TRUE)
+
+# Varianza
+
+# Definimos una función para la varianza
+variance <- function(x) {
+  n <- length(x, na.rm = TRUE)
+  return (n-1)*var(x, na.rm = TRUE)/n
+}
+
+homeruns_var <- variance(homeruns)
+salary_var <- variance(salary)
+
+# Desviación estándar
+
+homeruns_std = sqrt(var(homeruns, na.rm = TRUE))
+salary_std = sqrt(var(salary, na.rm = TRUE))
+
+# Rango intercuartílico
+
+homeruns_iqr <- IQR(homeruns, na.rm = TRUE)
+salary_iqr <- IQR(salary, na.rm = TRUE)
+
+#----------------------------------------------------------------------------------------------
+# Diagrama de cajas
+#----------------------------------------------------------------------------------------------
+
+boxplot(homeruns, range=1.5, main="Diagrama de cajas para el número de homeruns")
+boxplot(salary, range=1.5, main="Diagrama de cajas para el salario")
+
+#----------------------------------------------------------------------------------------------
+# Coeficientes de asimetría y curtosis
+#----------------------------------------------------------------------------------------------
+
+homeruns_skeness <- skewness(homeruns, na.rm = TRUE)
+salary_skeness <- skewness(salary, na.rm = TRUE)
